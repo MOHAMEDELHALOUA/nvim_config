be2139e1c5277c0e599895145cd607ef0d7017bf
ELHALOUAMOHAMED <mohamedelhaloua24@gmail.com>
2025-10-30 12:09:07 +0100

    initial commit, added basic nvim configuration with lazy, treesitter, telescope, and neotree, and create a separte plugins directory to store the plugins so that they will return only a table that will be used in init.lua file with only one line

---
+++ lua/plugins/Neotree.lua
@@ -0,0 +1,16 @@
+return{
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  config = function()
+    --To open Neotree:
+    vim.keymap.set('n', '<leader>fs', ':Neotree filesystem reveal left<CR>', {})
+    --To close Neotree:
+    vim.keymap.set('n', '<leader>fc', ':Neotree close left<CR>', {})
+  end
+}
