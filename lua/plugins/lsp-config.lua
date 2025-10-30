4a0e66f0b3e3ea8ca8f0701dfbcea51bc75d8eb3
ELHALOUAMOHAMED <mohamedelhaloua24@gmail.com>
2025-10-30 16:55:09 +0100

    Added LSP for lua, c/c++, python

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    version = "v1.10.0",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    version = "v1.29.0",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "clangd", "pyright"},
+        automatic_installation = true,
+      })
+    end,
+    dependencies = {'williamboman/mason.nvim'}
+  },
+  {
+    'neovim/nvim-lspconfig',
+    version = "v0.1.8",
+    dependencies = {'williamboman/mason-lspconfig.nvim'},
+    config = function()
+
+      local lspconfig = require("lspconfig")
+
+      require('lspconfig').lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {"vim"}
+            }
+          }
+        }
+      })
+
+      lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd' , vim.lsp.buf.definition, {})
+      vim.keymap.set({'n'}, '<leader>ac', vim.lsp.buf.code_action , {})
+    end
+  }
+}
